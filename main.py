9073b81080048ee98e81e555c30c456d46b70afc
NickJDel <nicholas.james.delaney@gmail.com>
2024-08-21 14:21:37 +1000

    added screen

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import pygame
 from constants import *
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
+    pygame.init()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, "black")
+        pygame.display.flip()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
